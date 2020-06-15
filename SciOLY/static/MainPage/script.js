9d51c4b9dc8fca776ea9e54eade25a97188856f8
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-06-15 12:52:46 -0500

    Added menu button functionality + js

---
+++ SciOLY/static/MainPage/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const MenuButton = document.querySelector(".Menu");
+
+    MenuButton.addEventListener('click', ()=>{
+        alert("Menu Button Clicked");
+    })
+})
