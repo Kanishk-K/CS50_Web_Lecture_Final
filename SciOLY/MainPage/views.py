8b519bae578c2edb2286f8ac39b20e842d598330
Kanishk-K <Kanishkkacholia@Gmail.com>
2020-06-15 11:30:07 -0500

    Added the ability to add templates to file

--- SciOLY/MainPage/views.py
+++ SciOLY/MainPage/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Begin Final Project")
+    return render(request, "MainPage/index.html")
